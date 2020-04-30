130b05ba9c864da9a66ad9ae12873d5e7889ad95
nishant666 <nishants440@gmail.com>
2020-04-30 23:18:15 +0530

    Updates

---
+++ precache-manifest.8664724a07768b83a977dde692080a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a78f566095b0ad9f8043dd88df1bc6e",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "226615f89241d69d162c",
+    "url": "/Currency-Converter/static/css/main.04a92892.chunk.css"
+  },
+  {
+    "revision": "5354fd51fdec1789f063",
+    "url": "/Currency-Converter/static/js/2.259dbfb2.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/Currency-Converter/static/js/2.259dbfb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226615f89241d69d162c",
+    "url": "/Currency-Converter/static/js/main.e82accb4.chunk.js"
+  },
+  {
+    "revision": "a764e3f9b674d84be491",
+    "url": "/Currency-Converter/static/js/runtime-main.2326b8b7.js"
+  }
+]);
